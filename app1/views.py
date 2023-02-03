cfb1cb29a0e7b2eb2612978efeec048c51aa728f
anchal0508 <anchal.koshta007@gmail.com>
2023-02-03 08:58:39 +0530

    Changed

--- app1/views.py
+++ app1/views.py
@@ -5,14 +5,21 @@ from django.shortcuts import render, HttpResponse
 tasks = ['foo','bar','teo']
 
 class newTaskForm(forms.Form):
-    task = forms.CharField(label='New Task :')
+    tas = forms.CharField(label='New Task :', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    name = forms.CharField(label='Name :', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # name = forms.CharField(label='name :')
+    # Address = forms.CharField(label='Address :')
+
+    
 def  index(request):
     if request.method == "POST":
         fem = newTaskForm(request.POST)
         if fem.is_valid():
-            task = fem.cleaned_data['task']
+            task = fem.cleaned_data['tas']
+            # n = fem.cleaned_data['name']
+            # address = fem.cleaned_data['Address']
             tasks.append(task)
-            return HttpResponseRedirect(reverse('app1:home'))
+            return HttpResponseRedirect(reverse('app1:contact'))
         else:
             return render(request, 'app1/index.html',{
                 'form':fem
